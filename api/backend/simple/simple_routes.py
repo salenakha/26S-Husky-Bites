038ad2ad530a724c6b7eda146d3d4e0d21d7f897
clioleungg <cleungclio1012@gmail.com>
2026-04-20 02:01:23 -0400

    docker ready sql files, renamed db creation to huskybites, other database formatting and prep

--- api/backend/simple/simple_routes.py
+++ api/backend/simple/simple_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, current_app, redirect, url_for
-from backend.simple.playlist import sample_playlist_data
+from backend.db_connection import get_db
 from backend.ml_models import model01
 
 # This blueprint handles basic routes useful for testing and demonstration
